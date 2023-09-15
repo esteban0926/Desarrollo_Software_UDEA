902fbef7ac0acbdfb15aefe9e533705ff18dde7b
esteban0926 <111551659+esteban0926@users.noreply.github.com>
2023-09-14 23:08:21 -0500

    Add files via upload

---
+++ Sprint2-Desarrollo/src/main/java/Empleado.java
@@ -0,0 +1,50 @@
+public class Empleado {
+
+    private String nombre;
+    private String correo;
+    private String empresa ;
+    private String rol;
+
+    // Constructor
+    public Empleado(String nombre, String correo, String empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+        // Getter y setter
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
