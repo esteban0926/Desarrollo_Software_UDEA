e84e353f53cef360af806cca355764c9d6580823
esteban0926 <111551659+esteban0926@users.noreply.github.com>
2023-10-15 13:33:22 -0500

    Delete Spint3 directory

--- Spint3/src/main/java/com/udea/Spint3/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udea.Spint3.repository;
-
-import com.udea.Spint3.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
--- Spint3/src/main/java/com/udea/Spint3/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.udea.Spint3.repository;
-
-import com.udea.Spint3.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
