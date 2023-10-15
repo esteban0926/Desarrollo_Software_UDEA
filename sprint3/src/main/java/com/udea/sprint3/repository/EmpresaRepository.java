279138ba7c472af769d0ee98658992514db3615b
esteban0926 <111551659+esteban0926@users.noreply.github.com>
2023-10-15 13:36:45 -0500

    Delete sprint3 directory

--- sprint3/src/main/java/com/udea/sprint3/repository/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udea.sprint3.repository;
-
-import com.udea.sprint3.entity.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
-}
